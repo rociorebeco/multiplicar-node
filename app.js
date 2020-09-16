d63819e40eb598d94ca1039540d09486d508196a
Rocio <roci.rebeco@gmail.com>
2020-09-16 20:49:19 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+const { crearArchivo } = require('./multiplicar/multiplicar')
+
+// const fs = require('express');
+//const fs = require('./fs');
+
+let base = 6;
+console.log(process);
+
+//crearArchivo(base)
+//   .then(archivo = console.log(`Archivo creado: ${ archivo }`))
+// .catch(e => console.log(e));
